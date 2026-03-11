7a1175d7d891eaa871eb16a9ca1bbd673a7c053b
abhishekpgECE <abhishekpg10c@gmail.com>
2026-03-11 16:11:46 +0530

    Added all the codes till D9-S9

---
+++ src/com/advanced/collections/maps/FrequencyCount.java
@@ -0,0 +1,17 @@
+package com.advanced.collections.maps;
+
+import java.util.HashMap;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,12,5,4};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr){
+            // 1=2, 2=2, 3=1, 4=1 and so on it counts the frequency
+            map.put(num ,map.getOrDefault(num ,0)+1);
+
+        }
+        System.out.println(map);
+        System.out.println("count of 4:"+map.get(4));
+    }
+}
